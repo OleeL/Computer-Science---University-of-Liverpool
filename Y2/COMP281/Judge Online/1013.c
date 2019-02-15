ed9aca6b994041d4fd300dd93452bcb3763be905
Oliver Legg <oliglegg@hotmail.co.uk>
2019-02-15 16:58:30 +0000

    Finished this one

--- Y2/COMP281/Judge Online/1013.c
+++ Y2/COMP281/Judge Online/1013.c
@@ -1,24 +1,53 @@
+/*
+* Student ID: 201234567
+* Student Name: Oliver Legg
+* Email: O.Legg@student.liverpool.ac.uk
+*
+* User: sgolegg
+*
+* Problem ID: 1013
+* RunID: 24000
+* Result: Accepted
+*/
+
 #include <stdio.h>
+
+int input(int score);
+void output(int scoreBoundaries[]);
 
 int main(void)
 {
-    int *pointer_start, *pointer;
-    pointer = pointer_start;
-    &pointer = -1;
-    while (&pointer != 0)
+
+    // scoreBoundaries [ 0 = high, 1 = medium, 2 = low ]
+    int score;
+    int scoreBoundaries[3] = {0, 0, 0};
+    scanf("%d", &score);
+    do
     {
-        scanf("%d", pointer);
-        printf("grade = %d \n", *pointer);
-        printf("start pointer = %d \n", pointer_start);
-        printf("pointer = %d \n\n", pointer);
-        pointer++;
-    }
-
-
-
-    for (int *i = pointer_start; i <= pointer; i++)
-    {
-        printf("%d ", *i);
-    }
+        scoreBoundaries[input(score)]++;
+        scanf("%d", &score);
+    }while(score != 0);
+    output(scoreBoundaries);
+    
     return 0;
+}
+
+// returns the correct index to increment
+int input(int score)
+{       
+    if (score >= 85)
+        return 0;
+    else 
+        if (score >= 60 && score <= 84)
+            return 1;
+        else
+            return 2;
+}
+
+
+void output(int scoreBoundaries[])
+{
+    printf(">=85:%d\n", scoreBoundaries[0]);
+    printf("60-84:%d\n", scoreBoundaries[1]);
+    printf("<60:%d", scoreBoundaries[2]);
 }
